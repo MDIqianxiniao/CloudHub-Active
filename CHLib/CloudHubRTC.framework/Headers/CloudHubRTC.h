867ee057e26775c54782bad200af01335f9350b6
MDI <>
2021-04-23 17:22:21 +0800

    去掉CloudHub

--- CHLib/CloudHubRTC.framework/Headers/CloudHubRTC.h
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright (c) 2020 CloudHub. all rights reserved.
-//
-
-
-#import <CloudHubRTC/CloudHubEnumerates.h>
-#import <CloudHubRTC/CloudHubObjects.h>
-#import <CloudHubRTC/CloudHubRtcEngineDelegate.h>
-#import <CloudHubRTC/CloudHubRtcEngineKit.h>
